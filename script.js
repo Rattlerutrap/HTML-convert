264fc3798fd6d6b9f82ab70e26583d6868ce73a2
Rattlerutrap <dimaaupov2005@gmail.com>
2026-04-24 21:12:03 +0300

    em img a blockquote

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ function convertMarkdown()
     html = html.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
     html = html.replace(/__(.*?)__/, '<strong>$1</strong>')
 
+    html = html.replace(/\*(.*?)\*/g, '<em>$1</em>')
+    html = html.replace(/_(.*?)_/g, '<em>$1</em>')
+
+    html = html.replace(/\!\[(.*?)\]\((.*?)\)/g, '<img alt="$1" src="$2">')
+
+    html = html.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>')
+
+    html = html.replace(/^\s*> (.*)/gm, '<blockquote>$1</blockquote>')
+
     output.textContent = html
 
     preview.innerHTML = html
